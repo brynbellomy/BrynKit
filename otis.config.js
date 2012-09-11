19c10de45d9ebfbc76d00457477a750624c27635
Bryn Austin Bellomy <bryn@signals.io>
2012-09-11 10:29:44 -0400

    adding otis documentation config file

---
+++ otis.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  inDir: "./",
+  outDir: "../gh-pages",
+  index: "Bryn.h.html",
+  markdownEngine: "showdown",
+  onlyUpdated: false,
+  colourScheme: "friendly",
+  ignoreHidden: true,
+  sidebarState: true,
+  tolerant: false,
+  exclude: "otis.config.js,README.md,podspecs,doc"
+};
